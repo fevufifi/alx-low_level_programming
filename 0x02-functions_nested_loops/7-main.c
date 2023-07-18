b4f9a0855f00c22a609d3e9c5c65af0d820a8473
fevufifi <fevenhaile156@gmail.com>
2023-07-18 15:59:08 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/7-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* main - check the code
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+int r;
+
+print_last_digit(98);
+rint_last_digit(0);
+r = print_last_digit(-1024);
+_putchar('0' + r);
+_putchar('\n');
+return (0);
+}
